6affa30f0497fdada8b24b58a0fbda743657d503
Thomas <123500706+quikstart86@users.noreply.github.com>
2024-02-19 22:34:20 +0000

    updated 'class' to 'className' for a few items

--- src/components/ProjectGallery.jsx
+++ src/components/ProjectGallery.jsx
@@ -7,147 +7,147 @@ const ProjectGallery = () => {
             <div className="row">
                 <div className="col-lg-4 col-sm-6 mt-4">
                     <div className="card">
-                        <img src="..." className="card-img-top" alt="..." />
+                    <i className="bi bi-file-earmark-x"></i>
+                    <img src="..." className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title fw-bold">Code Refactor</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk
                                 of the card's content.</p>
-                            <a href="#" className="btn btn-outline-dark">Go somewhere</a>
+                            <a href="https://github.com/quikstart86/horeison-code-refactor" target="_blank" className="btn btn-outline-dark">See Project</a>
                         </div>
                     </div>
                 </div>
                 <div className="col-lg-4 col-sm-6 mt-4">
                     <div className="card">
+                    <i className="bi bi-laptop"></i>
                         <img src="..." className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title fw-bold">HTML / CSS Portfolio</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk
                                 of the card's content.</p>
-                            <a href="#" className="btn btn-outline-dark">Go somewhere</a>
+                            <a href="https://github.com/quikstart86/ThomasJack-portfolio" target="_blank" className="btn btn-outline-dark">See Project</a>
                         </div>
                     </div>
                 </div>
                 <div className="col-lg-4 col-sm-6 mt-4">
                     <div className="card">
+                    <i className="bi bi-safe2"></i>
                         <img src="..." className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title fw-bold">Console Log</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk
                                 of the card's content.</p>
-                            <a href="#" className="btn btn-outline-dark">Go somewhere</a>
+                            <a href="https://github.com/quikstart86/Console-Finances" target="_blank" className="btn btn-outline-dark">See Project</a>
                         </div>
                     </div>
                 </div>
                 <div className="col-lg-4 col-sm-6 mt-4">
                     <div className="card">
+                    <i className="bi bi-lock"></i>
                         <img src="..." className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title fw-bold">Password Generator</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk
                                 of the card's content.</p>
-                            <a href="#" className="btn btn-outline-dark">Go somewhere</a>
+                            <a href="https://github.com/quikstart86/Random-Password-Generator" target="_blank" className="btn btn-outline-dark">See Project</a>
                         </div>
                     </div>
                 </div>
                 <div className="col-lg-4 col-sm-6 mt-4">
                     <div className="card">
-                        <img src="..." className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title fw-bold">Code Quizz</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk
-                                of the card's content.</p>
-                            <a href="#" className="btn btn-outline-dark">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-lg-4 col-sm-6 mt-4">
-                    <div className="card">
+                    <i className="bi bi-calendar-check"></i>
                         <img src="..." className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title fw-bold">Day Scheduler</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk
                                 of the card's content.</p>
-                            <a href="#" className="btn btn-outline-dark">Go somewhere</a>
+                            <a href="https://github.com/quikstart86/TJ---Work-Day-Scheduler" target="_blank" className="btn btn-outline-dark">See Project</a>
                         </div>
                     </div>
                 </div>
                 <div className="col-lg-4 col-sm-6 mt-4">
                     <div className="card">
+                    <i className="bi bi-cloud-drizzle-fill"></i>
                         <img src="..." className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title fw-bold">Weather Dashboard</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk
                                 of the card's content.</p>
-                            <a href="#" className="btn btn-outline-dark">Go somewhere</a>
+                            <a href="https://github.com/quikstart86/TJ-Weather-Dashboard" target="_blank" className="btn btn-outline-dark">See Project</a>
                         </div>
                     </div>
                 </div>
                 <div className="col-lg-4 col-sm-6 mt-4">
                     <div className="card">
+                    <i className="bi bi-terminal"></i>
                         <img src="..." className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title fw-bold">Readme Generator</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk
                                 of the card's content.</p>
-                            <a href="#" className="btn btn-outline-dark">Go somewhere</a>
+                            <a href="https://github.com/quikstart86/TJ-Readme-Generator" target="_blank" className="btn btn-outline-dark">See Project</a>
                         </div>
                     </div>
                 </div>
                 <div className="col-lg-4 col-sm-6 mt-4">
                     <div className="card">
+                    <i className="bi bi-laptop"></i>
                         <img src="..." className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title fw-bold">Bootstrap Portfolio</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk
                                 of the card's content.</p>
-                            <a href="#" className="btn btn-outline-dark">Go somewhere</a>
+                            <a href="https://github.com/quikstart86/Bootstrap-Portfolio" target="_blank" className="btn btn-outline-dark">See Project</a>
                         </div>
                     </div>
                 </div>
                 <div className="col-lg-4 col-sm-6 mt-4">
                     <div className="card">
+                    <i className="bi bi-terminal"></i>
                         <img src="..." className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title fw-bold">Profile Generator</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk
                                 of the card's content.</p>
-                            <a href="#" className="btn btn-outline-dark">Go somewhere</a>
+                            <a href="https://github.com/quikstart86/TJ-Team-Profile-Generator" target="_blank" className="btn btn-outline-dark">See Project</a>
                         </div>
                     </div>
                 </div>
                 <div className="col-lg-4 col-sm-6 mt-4">
                     <div className="card">
-                        <img src="..." className="card-img-top" alt="..." />
+                    <i className="bi bi-laptop"></i>
+                        <img src="#" className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title fw-bold">React Portfolio</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk
                                 of the card's content.</p>
-                            <a href="#" className="btn btn-outline-dark">Go somewhere</a>
+                            <a href="https://github.com/quikstart86/TJ-React-Portfolio" target="_blank" className="btn btn-outline-dark">See Project</a>
                         </div>
                     </div>
                 </div>
                 <div className="col-lg-4 col-sm-6 mt-4">
                     <div className="card">
-                        <img src="..." className="card-img-top" alt="..." />
+                        <img src="https://www.cbioe.com/wp-content/uploads/2022/11/cropped-Final-logo_transparent-1.png" className="card-img-top py-1" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title fw-bold">Carousel Bio Energy</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk
                                 of the card's content.</p>
-                            <a href="#" className="btn btn-outline-dark">Go somewhere</a>
+                            <a href="https://www.cbioe.com/" target="_blank" className="btn btn-outline-dark">See Project</a>
                         </div>
                     </div>
                 </div>
                 <div className="col-lg-4 col-sm-6 mt-4">
                     <div className="card">
-                        <img src="..." className="card-img-top" alt="..." />
+                        <img src="https://carouselfinance.com/wp-content/uploads/2023/02/cropped-Full-Logo_Colour.png" className="card-img-top py-3" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title fw-bold">Carousel Finance</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk
                                 of the card's content.</p>
-                            <a href="#" className="btn btn-outline-dark">Go somewhere</a>
+                            <a href="https://carouselfinance.com/" target="_blank" className="btn btn-outline-dark">See Project</a>
                         </div>
                     </div>
                 </div>
+                
             </div>
         </div>
     </section>
