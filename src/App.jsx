ddf994dd23db2919f1cd61a24ae18afb5c3975a5
Thomas <123500706+quikstart86@users.noreply.github.com>
2024-02-20 18:24:43 +0000

    changed from 'BrowserRouter to hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import Home from "./components/Home.jsx";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import ProjectGallery from "./components/ProjectGallery.jsx";
 import Contact from "./components/Contact.jsx";
 import "./App.css"
